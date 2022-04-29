e13f552359b242757bf4c23f9dfbe356a067f89d
RomaSamkov <Romansamkov.rs@gmail.com>
2022-04-29 11:55:36 +0300

    add styless

--- src/index.js
+++ src/index.js
@@ -22,24 +22,22 @@ function onSearch(event) {
     }
 };
 function createMurkupList(country) {
-    const elMurkup = `<li class="country-list-item"><img src = '${country.flags.svg}' width = '30' alt=${country.name.common}>${country.name.common}</li>`;
-    return elMurkup;
+    return `<li class="country-list__item"><img src = '${country.flags.svg}' width = '30' alt=${country.name.common}>${country.name.common}</li>`;
 }
 
 function createMurkupInfo(country) {
-    const elMurkup = `<h2 class="country-info__title"><img class="country-info__flag" src="${country.flags.svg}" alt=${country.name.common}/>${country.name.common}</h2>
+    return `<h2 class="country-info__title"><img class="country-info__flag" src="${country.flags.svg}" alt=${country.name.common}/>${country.name.common}</h2>
         <ul class="country-info__list">
-          <li class="country-info__list-item">
+          <li class="country-info__item">
             <span class="country-info__name">Capital: </span>${country.capital}
           </li>
-          <li class="country-info__list-item">
+          <li class="country-info__item">
             <span class="country-info__name">Population: </span>${country.population}
           </li>
-          <li class="country-info__list-item">
+          <li class="country-info__item">
             <span class="country-info__name">Languages: </span>${Object.values(country.languages).join(',')}
           </li>
         </ul>`;
-    return elMurkup;
 };
 
 function onMurkupList(country) {
